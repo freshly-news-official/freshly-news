ba4cae7ee6c73e0201cc7516c23ea528d751b2ca
Alexandru <alexandru.elisei@gmail.com>
2014-12-13 19:16:05 +0200

    added devise User table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
     root 'fresh#index'
+
     post '/search'  => 'fresh#search'
     get '/news'    => 'fresh#news'
     get '/top_news' => 'fresh#top_news'
